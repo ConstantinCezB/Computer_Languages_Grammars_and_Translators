d65dbc10aab6ea4e4166b0edcb233a3bda436189
Constantin Cezar Begu <38817297+ConstantinCezB@users.noreply.github.com>
2019-01-18 12:21:11 -0500

    Final submission

--- src/Assignment_1/A11.java
+++ src/Assignment_1/A11.java
@@ -1,30 +1,23 @@
 package Assignment_1;
 
-import java.io.*;
+import java.nio.file.*;
 import java.util.*;
 
 public class A11 {
-    public static  void main (String[] args) throws Exception {
-        String content = new Scanner(new File("filename")).useDelimiter("\\Z").next();
-        StringTokenizer st = new StringTokenizer(content, " !@#$%^&*()_+-=:;<>,.?/'{}[]|\\~`");
+    public static void main(String[] args) throws Exception {
+        String content = new String(Files.readAllBytes(Paths.get(args[0])));
+        StringTokenizer st = new StringTokenizer(content, " !@#$%^&*()_+-=:;<>,?/'{}[]|\\~`\n");
         LinkedHashSet<String> identifiersUsed = new LinkedHashSet<>(Arrays.asList("WRITE", "READ", "IF", "ELSE", "RETURN", "BEGIN", "END", "MAIN", "STRING", "INT", "REAL"));
-        while (st.hasMoreTokens()){
+        while (st.hasMoreTokens()) {
             String temp = st.nextToken();
-            if (!identifiersUsed.contains(temp) && isAlphaAndNum(temp)) {
-                identifiersUsed.add(temp);
-            }
+            if (isAlphaAndNum(temp)) identifiersUsed.add(temp);
         }
-        System.out.println(identifiersUsed.size() - 11);
-
+        Files.write(Paths.get("./A1.output"), ("Identifiers: " + (identifiersUsed.size() - 11)).getBytes());
     }
-    @SuppressWarnings("Duplicates")
+
     private static boolean isAlphaAndNum(String name) {
         char[] chars = name.toCharArray();
-        for (char c : chars) {
-            if (Character.isLetter(name.charAt(0)) && (!(Character.isLetter(c) || Character.isDigit(c)))) {
-                return false;
-            }
-        }
+        for (char c : chars) if (!(Character.isLetter(c) || Character.isDigit(c))) return false;
         return true;
     }
 }
--- src/Assignment_1/A12.java
+++ src/Assignment_1/A12.java
@@ -1,4 +1,16 @@
 package Assignment_1;
 
+import java.nio.file.*;
+import java.util.*;
+import java.util.regex.*;
+
 public class A12 {
+    public static void main(String[] args) throws Exception {
+        String content = new String(Files.readAllBytes(Paths.get(args[0])));
+        Pattern p = Pattern.compile("([a-zA-Z][a-zA-Z0-9]*)|(\"[^\"]*\")");
+        LinkedHashSet<String> identifiersUsed = new LinkedHashSet<>(Arrays.asList("WRITE", "READ", "IF", "ELSE", "RETURN", "BEGIN", "END", "MAIN", "STRING", "INT", "REAL"));
+        Matcher m = p.matcher(content);
+        while (m.find()) if (!m.group().startsWith("\"")) identifiersUsed.add(m.group());
+        Files.write(Paths.get("./A1.output"), ("Identifiers: " + (identifiersUsed.size() - 11)).getBytes());
+    }
 }
